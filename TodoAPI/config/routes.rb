9f349928bf2412f10fc5944426b036e9aa7cfe9f
Guilherme Berger <guilherme.berger@gmail.com>
2014-02-17 23:24:40 -0300

    API: Namespace the API URL with /api/v1.

--- TodoAPI/config/routes.rb
+++ TodoAPI/config/routes.rb
@@ -1,3 +1,7 @@
 TodoAPI::Application.routes.draw do
-  resources :todos, except: [:new, :edit]
+  namespace :api do
+    namespace :v1 do
+      resources :todos, except: [:new, :edit]
+    end
+  end
 end
