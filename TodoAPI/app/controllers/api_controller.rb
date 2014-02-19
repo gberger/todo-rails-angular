ce59317a224a69f262b3c0b5b49eb87855e39a4f
Guilherme Berger <guilherme.berger@gmail.com>
2014-02-19 00:17:52 -0300

    API: Receive access_token from header or from param.

--- TodoAPI/app/controllers/api_controller.rb
+++ TodoAPI/app/controllers/api_controller.rb
@@ -1,6 +1,14 @@
 class ApiController < ApplicationController
+  def provided_access_token
+    params[:access_token] || request.headers['AccessToken'] || nil
+  end
+
   def current_user
-    @current_user ||= (api_key = ApiKey.find_by_access_token(params[:access_token])) && !api_key.expired? && api_key.user
+    @current_user ||= !!current_api_key ? current_api_key.user : nil
+  end
+
+  def current_api_key
+    @api_key ||= ApiKey.find_by_access_token(provided_access_token)
   end
 
   before_filter :restrict_access
